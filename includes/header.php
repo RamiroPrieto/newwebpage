6b6fed1c2b8a668f3c4bcc3f280ad8c47a2687e7
Matias Araneta <maraneta@medicosonline.com.ar>
2023-11-25 18:37:20 -0300

    fasfas

--- includes/header.php
+++ includes/header.php
@@ -1,50 +1,90 @@
 <header class="fusion-header-wrapper">
-				<div class="fusion-header-v1 fusion-logo-alignment fusion-logo-left fusion-sticky-menu- fusion-sticky-logo- fusion-mobile-logo-  fusion-mobile-menu-design-modern">
-					<div class="fusion-header-sticky-height"></div>
-<div class="fusion-header">
-	<div class="fusion-row">
-					<div class="fusion-logo" data-margin-top="25px" data-margin-bottom="25px" data-margin-left="0px" data-margin-right="0px">
-			<a class="fusion-logo-link" href="./index.php">
+	<div
+		class="fusion-header-v1 fusion-logo-alignment fusion-logo-left fusion-sticky-menu- fusion-sticky-logo- fusion-mobile-logo-  fusion-mobile-menu-design-modern">
+		<div class="fusion-header-sticky-height"></div>
+		<div class="fusion-header">
+			<div class="fusion-row">
+				<div class="fusion-logo" data-margin-top="25px" data-margin-bottom="25px" data-margin-left="0px"
+					data-margin-right="0px">
+					<a class="fusion-logo-link" href="./index.php">
 
 						<!-- standard logo -->
-			<img src="./assets/img/BUGLE.png" srcset="./assets/img/BUGLE.png 1x, ./assets/img/BUGLE.png 2x" width="99" height="26" style="max-height:26px;height:auto;" alt="越境ECプラットフォーム / Inagora株式会社 ロゴ" data-retina_logo_url="https://www.inagora.com/wp-content/uploads/2017/12/brand-logo_1@2x.png" class="fusion-standard-logo"/>
+						<img src="./assets/img/BUGLE.png" srcset="./assets/img/BUGLE.png 1x, ./assets/img/BUGLE.png 2x"
+							width="99" height="26" style="max-height:26px;height:auto;"
+							alt="越境ECプラットフォーム / Inagora株式会社 ロゴ"
+							data-retina_logo_url="https://www.inagora.com/wp-content/uploads/2017/12/brand-logo_1@2x.png"
+							class="fusion-standard-logo" />
 
-			
+
 					</a>
-		</div>		<nav class="fusion-main-menu" aria-label="Main Menu"><div class="fusion-overlay-search">		<form role="search" class="searchform fusion-search-form  fusion-search-form-classic" method="get" action="https://www.inagora.com/">
-			<div class="fusion-search-form-content">
-
-				
-				<div class="fusion-search-field search-field">
-					<label><span class="screen-reader-text">検索 …</span>
-													<input type="search" value="" name="s" class="s" placeholder="検索..." required aria-required="true" aria-label="検索..."/>
-											</label>
 				</div>
-				<div class="fusion-search-button search-button">
-					<input type="submit" class="fusion-search-submit searchsubmit" aria-label="検索" value="&#xf002;"/>
-									</div>
+				<nav class="fusion-main-menu" aria-label="Main Menu">
+					<div class="fusion-overlay-search">
+						<form role="search" class="searchform fusion-search-form  fusion-search-form-classic"
+							method="get" action="https://www.inagora.com/">
+							<div class="fusion-search-form-content">
 
-				
-			</div>
+
+								<div class="fusion-search-field search-field">
+									<label><span class="screen-reader-text">検索 …</span>
+										<input type="search" value="" name="s" class="s" placeholder="検索..." required
+											aria-required="true" aria-label="検索..." />
+									</label>
+								</div>
+								<div class="fusion-search-button search-button">
+									<input type="submit" class="fusion-search-submit searchsubmit" aria-label="検索"
+										value="&#xf002;" />
+								</div>
 
 
-			
-		</form>
-		<div class="fusion-search-spacer"></div><a href="#" role="button" aria-label="Close Search" class="fusion-close-search"></a></div><ul id="menu-main-menu" class="fusion-menu"><li id="menu-item-13688" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-13688" data-item-id="13688"><a href="./company.php" class="fusion-bar-highlight"><span class="menu-text">企業情報</span></a></li><li id="menu-item-14235" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-14235 fusion-dropdown-menu" data-item-id="14235"><a href="./business.php" class="fusion-bar-highlight"><span class="menu-text">事業情報</span> <span class="fusion-caret"></span></a><ul class="sub-menu"><li id="menu-item-14203" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14203 fusion-dropdown-submenu"><a href="./business.php" class="fusion-bar-highlight"><span>事業内容｜越境EC事業</span></a></li>
-        <li id="menu-item-14205" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14205 fusion-dropdown-submenu"><a href="brand_incubation" class="fusion-bar-highlight"><span>ブランドインキュベーション事業</span></a></li>
-            <li id="menu-item-15222" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-15222 fusion-dropdown-submenu"><a href="southeast_asia_taiwan" class="fusion-bar-highlight"><span>東南アジア・台湾事業</span></a></li><li id="menu-item-15826" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-15826 fusion-dropdown-submenu"><a href="inbound_business" class="fusion-bar-highlight"><span>インバウンド・地方創生</span></a></li></ul></li><li id="menu-item-11598" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11598" data-item-id="11598"><a href="crossborder" class="fusion-bar-highlight"><span class="menu-text">市場環境</span></a></li>
-           
-            <li id="menu-item-8609" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8609 fusion-menu-item-button" data-item-id="8609"><a href="contact" class="fusion-bar-highlight"><span class="menu-text fusion-button button-default button-medium">お問い合わせ</span></a></li></ul></nav>	<div class="fusion-mobile-menu-icons">
-							<a href="#" class="fusion-icon awb-icon-bars" aria-label="Toggle mobile menu" aria-expanded="false"></a>
-		
-		
-		
-			</div>
+							</div>
 
-<nav class="fusion-mobile-nav-holder fusion-mobile-menu-text-align-left" aria-label="Main Menu Mobile"></nav>
 
+
+						</form>
+						<div class="fusion-search-spacer"></div><a href="#" role="button" aria-label="Close Search"
+							class="fusion-close-search"></a>
 					</div>
-</div>
+					<ul id="menu-main-menu" class="fusion-menu">
+						<li id="menu-item-13688"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-13688"
+							data-item-id="13688"><a href="./company.php" class="fusion-bar-highlight"><span
+									class="menu-text">企業情報</span></a></li>
+						<li id="menu-item-14235"
+							class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-14235 fusion-dropdown-menu"
+							data-item-id="14235"><a href="./business.php" class="fusion-bar-highlight"><span
+									class="menu-text">事業情報</span> <span class="fusion-caret"></span></a>
+							<ul class="sub-menu">
+								<li id="menu-item-14203"
+									class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14203 fusion-dropdown-submenu">
+									<a href="./business.php" class="fusion-bar-highlight"><span>事業内容｜越境EC事業</span></a>
+								</li>
+								<li id="menu-item-15222"
+									class="menu-item menu-item-type-post_type menu-item-object-page menu-item-15222 fusion-dropdown-submenu">
+									<a href="southeast_asia_taiwan"
+										class="fusion-bar-highlight"><span>東南アジア・台湾事業</span></a></li>
+							</ul>
+						</li>
+
+						<li id="menu-item-8609"
+							class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8609 fusion-menu-item-button"
+							data-item-id="8609"><a href="contact" class="fusion-bar-highlight"><span
+									class="menu-text fusion-button button-default button-medium">お問い合わせ</span></a></li>
+					</ul>
+				</nav>
+				<div class="fusion-mobile-menu-icons">
+					<a href="#" class="fusion-icon awb-icon-bars" aria-label="Toggle mobile menu"
+						aria-expanded="false"></a>
+
+
+
 				</div>
-				<div class="fusion-clearfix"></div>
-			</header>
+
+				<nav class="fusion-mobile-nav-holder fusion-mobile-menu-text-align-left" aria-label="Main Menu Mobile">
+				</nav>
+
+			</div>
+		</div>
+	</div>
+	<div class="fusion-clearfix"></div>
+</header>
